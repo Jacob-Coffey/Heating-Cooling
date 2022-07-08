7c42a6887d4d9d8b97f27a1dcb3fed1ccaba0496
Jacob Coffey <Jacob23.coffey@gmail.com>
2022-07-08 13:05:55 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let actualTemp = 80;
+let desiredTemp = 80;
+
+if(actualTemp > desiredTemp){
+    console.log("Run A/C");
+} else if(actualTemp < desiredTemp){
+    console.log("Run Heat");
+} else{
+    console.log("Standy");
+}
